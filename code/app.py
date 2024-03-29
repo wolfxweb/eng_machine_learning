e7dcf67cae9ff187a8efec481dcdad6b1a337da5
Eduardo lobo <63437120+wolfxweb@users.noreply.github.com>
2024-03-29 19:30:20 -0300

    Update app.py

--- code/app.py
+++ code/app.py
@@ -8,17 +8,16 @@ def main():
     # Aba PreparacaoDados
     with st.sidebar:
         st.sidebar.title('Menu')
-        tab_selected = st.radio('Selecione uma aba:', ['PreparacaoDados', 'Treinamento', 'PipelineAplicacao'])
+        tab_selected = st.radio('Selecione uma aba:', ['Preparacao Dados', 'Treinamento', 'Pipeline Aplicacao','  Apresentação'])
   
 
     if tab_selected == 'PreparacaoDados':
         st.header('Preparação de Dados')
         st.write('Este é um exemplo de aplicação Streamlit com a aba de Preparação de Dados.')
-      # URL direta da imagem hospedada no GitHub
-        image_url = f"{url_grafico}/boxplot_faixa_dinamica.png"
-
-        # Exibir a imagem no Streamlit
-        st.image(image_url, caption='Exemplo de imagem na preparação de dados', use_column_width=True)
+     
+        # URL direta da imagem hospedada no GitHub
+        boxplot_faixa_dinamica = f"{url_grafico}/boxplot_faixa_dinamica.png"
+        st.image(boxplot_faixa_dinamica, caption='Exemplo de imagem na preparação de dados', use_column_width=True)
 
     # Aba Treinamento
     elif tab_selected == 'Treinamento':
@@ -29,6 +28,24 @@ def main():
     elif tab_selected == 'PipelineAplicacao':
         st.header('Pipeline de Aplicação')
         st.write('Este é um exemplo de aplicação Streamlit com a aba de Pipeline de Aplicação.')
+    
+    elif tab_selected == 'Apresentação':
+        st.header('Apresentação')
+        st.write('Este é um exemplo de aplicação Streamlit com a aba de Pipeline de Aplicação.')
+        # URL do arquivo PDF hospedado no OneDrive
+        pdf_url = "https://1drv.ms/p/s!AtgitvPT0788g_lFaG6r4kMmh8Jdqg?e=QG7SK4"
+
+        # Exibir o PDF usando um iframe
+        st.write(f'<iframe src="{pdf_url}" width="700" height="500"></iframe>', unsafe_allow_html=True)
+
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     main()

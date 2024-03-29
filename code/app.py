f7b752a121b3e97732931bc6ec14537a831081b3
Eduardo lobo <63437120+wolfxweb@users.noreply.github.com>
2024-03-29 19:32:39 -0300

    Update app.py

--- code/app.py
+++ code/app.py
@@ -16,7 +16,7 @@ def main():
         st.write('Este é um exemplo de aplicação Streamlit com a aba de Preparação de Dados.')
      
         # URL direta da imagem hospedada no GitHub
-        boxplot_faixa_dinamica = f"{url_github}/graficos/pre_processamento/boxplot_faixa_dinamica.png"
+        boxplot_faixa_dinamica = "https://github.com/wolfxweb/eng_machine_learning/raw/f0dd74b77a240c522383b7ef104bce11d61ae17c/docs/artefatos/graficos/pre_processamento/boxplot_faixa_dinamica.png"
         st.image(boxplot_faixa_dinamica, caption='Exemplo de imagem na preparação de dados', use_column_width=True)
 
     # Aba Treinamento
